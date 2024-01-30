22f5e9386142c54a192f9ec13d4a830bbf551514
Le Marc <lemarc.dev@gmail.com>
2024-01-30 23:05:28 +0100

    add scripts by src

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { resolve } from "path";
 import { $ } from "bun";
 import { watch } from "fs";
 const router = new Bun.FileSystemRouter({
@@ -18,6 +17,17 @@ type RouteObj = {
 	"client.ts"?: string;
 	"index.ts"?: string;
 	"layout.ts"?: string;
+};
+const getRelatedPath = (file: string) => {
+	const splitted = file.split("/");
+	const idx = splitted.findIndex((v) => v === "dist");
+	if (idx !== -1) {
+		for (let i = 0; i <= idx; i++) {
+			splitted[i] = "";
+		}
+	}
+	const scriptSrc = splitted.filter(Boolean).join("/");
+	return scriptSrc;
 };
 const builder = async (input: string, outPath: string) => {
 	const output = await Bun.build({
@@ -50,7 +60,10 @@ const builder = async (input: string, outPath: string) => {
 	const path = input.split("/").filter((v) => !isDot(v) && v !== "pages");
 	if (path.length === 1) {
 		if (isClient(input)) {
-			fileMap[path[0]] = await Bun.file(output.outputs[0].path).text();
+			const scriptSrc = getRelatedPath(output.outputs[0].path);
+
+			fileMap[path[0]] = scriptSrc;
+
 			return;
 		}
 		fileMap[path[0]] = output.outputs[0].path;
@@ -61,7 +74,9 @@ const builder = async (input: string, outPath: string) => {
 			const file = output.outputs[0].path;
 			if (isLast) {
 				if (isClient(input)) {
-					link["client.ts"] = await Bun.file(file).text();
+					const scriptSrc = getRelatedPath(file);
+
+					link["client.ts"] = scriptSrc;
 					return;
 				}
 				if (isServer(input)) {
@@ -93,7 +108,6 @@ const filterFolders = (list: string[]) =>
 
 const process = async (path = "./pages", outpath = "./dist") => {
 	const list = (await allNamesInFolder(path)).filter(Boolean);
-	//console.log(`list in path = ${path} and list = ${list}`);
 	if (list.length) {
 		const jsFiles = filterTsFiles(list);
 		for (const file of jsFiles) {
@@ -114,43 +128,31 @@ const watcher = watch(
 	{ recursive: true },
 	async (event, filename) => {
 		const splitted = filename?.split("/");
-		if (splitted?.at(-1) === "layout.ts") {
-			console.log("layout should be rebuilded");
-		}
+		// if (splitted?.at(-1) === "layout.ts") {
+		// 	console.log("layout should be rebuilded");
+		// }
 		if (splitted) {
 			splitted.pop();
 			const outdir = splitted.join("/");
 			await builder(`./pages/${filename}`, `./dist/${outdir}`);
-
-			console.log(`Detected ${event} in ${filename}`);
+			//console.log(`Detected ${event} in ${filename}`);
 		}
 	},
 );
 
 const buildTemplate = async (path: string[]) => {
 	let currentTemplate = "";
-
 	let link = fileMap;
-
 	for (let i = 0; i < path.length; i++) {
 		const chunk = path[i];
-		// console.log("--------------------------");
-		// console.log("chunk=", chunk);
-		// console.log("--------------------------");
 		const isInnerLayout = link[chunk] && link["layout.ts"];
-		// console.log("isInnerLayout=", isInnerLayout);
 		if (isInnerLayout) {
 			const innerLayout = await (await import(link["layout.ts"])).server();
-			// console.log("innerLayout = ", innerLayout);
-			// console.log("--------------------------");
-			// console.log("currentTemplate = ", currentTemplate);
 			if (currentTemplate) {
 				currentTemplate = currentTemplate.replace("<!--slot-->", innerLayout);
 			} else {
 				currentTemplate = innerLayout;
 			}
-			// console.log("--------------------------");
-			// console.log("currentTemplate after replace = ", currentTemplate);
 		}
 
 		link = link[chunk];
@@ -160,9 +162,10 @@ const buildTemplate = async (path: string[]) => {
 const getPageFiles = async (path: string[]) => {
 	let link = fileMap;
 	const clientScripts: string[] = [];
+	let currentPath = "";
 	for (const chunk of path) {
+		currentPath += chunk;
 		if (link["client.ts"]) {
-			console.log("there is client script");
 			clientScripts.push(link["client.ts"]);
 		}
 		if (!link[chunk] || chunk.endsWith(".ts")) {
@@ -177,75 +180,43 @@ const getPageFiles = async (path: string[]) => {
 	}
 	return null;
 };
-// console.log("fileMap= ", fileMap);
+
 Bun.serve({
 	port: 3000,
 	async fetch(req) {
-		//console.log("req.url = ", req.url.replace(base, "") || "/");
-
-		const path = router.match(req.url.replace(base, "") || "/");
-		//console.log(path);
+		const replaced = req.url.replace(base, "");
+		const path = router.match(replaced || "/");
 
 		if (path) {
 			//@ts-ignore
 			const splittedPath = path.scriptSrc.split("/");
-			// console.log("splittedPath =", splittedPath);
 			// biome-ignore lint/style/noNonNullAssertion: <explanation>
 			const data = await getPageFiles(splittedPath)!;
 			if (data) {
 				const { clientScripts, server } = data;
 				const layout = await buildTemplate(splittedPath);
 				const replacedLayout = layout.replace("<!--slot-->", server);
-				// console.log("server = ", server);
-				// console.log("layout = ", layout);
-				// console.log("replacedLayout = ", replacedLayout);
-				// console.log("client= ", client);
 				const scriptsWrapped =
 					clientScripts
-						.map((client) => `<script>${client}</script>`)
+						.map((client) => `<script src=${base}/${client} defer></script>`)
 						.join("") || "";
-				console.log("length = ", scriptsWrapped.length);
-				console.log("scriptsWrapped= ", scriptsWrapped);
+
 				const html = template
 					// .replace("<!--app-head-->", rendered.head ?? "")
 					.replace("<!--app-html-->", replacedLayout ?? "")
 					.replace("<!--app-client-->", scriptsWrapped);
 				return new Response(html, { headers: { "Content-type": "text/html" } });
 			}
-
-			// const searchString = "pages/";
-			// const index = path.filePath.indexOf(searchString);
-			// console.log("index = ", index);
-			// if (index !== -1) {
-			// 	const resultString = path?.pathname.substring(
-			// 		index + searchString.length,
-			// 	);
-			// 	console.log("resultString = ", resultString);
-			// }
-			// const pathToDist = path.filePath.replace("pages", "dist");
-			// console.log("pathToDist = ", pathToDist);
-
-			// const layoutFile = pathToDist.replace("index.ts", "layout.js");
-			// const pageFile = pathToDist.replace(".ts", ".js");
-			// const clientFile = pathToDist.replace("index.ts", "client.js");
-			// const { server: layout } = await import(layoutFile);
-			// const { server: page } = await import(pageFile);
-			// const client = await Bun.file(clientFile).text();
-			// const rendered = await page();
-			// const withTemplate = layout().replace("<slot />", rendered);
-
-			// const html = template
-			// 	// .replace("<!--app-head-->", rendered.head ?? "")
-			// 	.replace("<!--app-html-->", withTemplate ?? "")
-			// 	.replace(
-			// 		"<!--app-client-->",
-			// 		client ? `<script>${client.toString()}</script>` : "",
-			// 	);
-
-			// // console.log(html);
-			// return new Response(html, { headers: { "Content-type": "text/html" } });
 		}
+		if (req.url.endsWith(".js")) {
+			req.url.split("/");
+			const file = await Bun.file(`./dist${replaced}`).text();
+
+			return new Response(file, {
+				headers: { "Content-type": "application/javascript" },
+			});
+		}
+		req.referrer;
 		return new Response("404!");
 	},
 });
-// <script type="module" src="/src/entry-client.ts"></script>
